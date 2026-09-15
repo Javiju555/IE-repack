7fe21e34bea0aa3d27ad8dd09c5e9ec0ca856d95
Javiju <185687643+Javiju555@users.noreply.github.com>
2026-09-16 01:33:00 +0200

    Nombre propio en Linux: app_id + .desktop en la release

--- gui/crates/ie-gui/src/main.rs
+++ gui/crates/ie-gui/src/main.rs
@@ -9,7 +9,8 @@ fn main() -> eframe::Result<()> {
     let opts = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([780.0, 600.0])
-            .with_min_inner_size([640.0, 500.0]),
+            .with_min_inner_size([640.0, 500.0])
+            .with_app_id("ie-repack"),
         ..Default::default()
     };
     eframe::run_native("IE Repack", opts, Box::new(|_| Ok(Box::new(App::default()))))
